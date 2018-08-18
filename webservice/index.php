ddebb05c2cd3b4f4f606a3d46998815c8d31546f
Flavia Rangel <flavinharps@gmail.com>
2018-08-18 13:39:16 -0300

    json_decode

--- webservice/index.php
+++ webservice/index.php
@@ -1,6 +1,16 @@
 <?php
-// Recebe uma string no formato JSON
-	$usuario = "{ nome: 'Fulano da Silva', ativo: true, nascimento: '2009/05/12' }";
-$meuUsuario = json_decode($usuario, true);
-echo $meuUsuario['nome']; // Fulano da Silva
-echo $meuUsuario['nascimento']; // 2009/05/12
+
+	require_once('config.php');
+	require_once('info.json');
+	
+	$obj = json_decode($json_str);
+
+	echo "Titulo: $obj->titulo<br>"; 
+	echo "Autor: $obj->autor<br>"; 
+	echo "ISBN13: $obj->isbn13<br>"
+	echo "ano": $obj->ano;
+	
+	mysql_query("INSERT INTO livros (titulo, autor, isbn13, ano) VALUES ($obj->titulo, $obj->autor, $obj->isbn13, $obj->ano)");
+
+?>
+
